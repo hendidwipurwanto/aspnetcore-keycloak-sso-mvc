6f6d00ae8113eb6d4c6b01971af3e3515cb30225
hendi.dp <hendi.dp@pttmg.com>
2026-01-06 15:28:30 +0700

    added AccountController

---
+++ KeycloakSso.Mvc.sln/Web.Mvc/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Mvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            return SignOut(
+                new AuthenticationProperties
+                {
+                    RedirectUri = "/"
+                },
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                OpenIdConnectDefaults.AuthenticationScheme
+            );
+        }
+    }
+}
