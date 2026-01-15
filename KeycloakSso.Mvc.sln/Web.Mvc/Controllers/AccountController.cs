c82fd7b6cb0355f816c6304cb162fbd09baf4853
hendi.dp <hendi.dp@pttmg.com>
2026-01-15 10:16:50 +0700

    move login and logout in AccountController

--- KeycloakSso.Mvc.sln/Web.Mvc/Controllers/AccountController.cs
+++ KeycloakSso.Mvc.sln/Web.Mvc/Controllers/AccountController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Web.Core.Services.Abstractions;
 
 namespace Web.Mvc.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
+        private readonly ICurrentUser _currentUser;
+        public AccountController( ICurrentUser currentUser)
+        {
+            _currentUser = currentUser;
+        }
+        public IActionResult Login()
+        {
+            return Content($"Hello {_currentUser.Username}");
+        }
         [HttpPost]
         public IActionResult Logout()
         {
